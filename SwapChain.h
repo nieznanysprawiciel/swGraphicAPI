51854d0b3f248d97e4db3f036c054877e8ce484f
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2016-09-28 23:48:31 +0200

    EngineGUI can display native window

--- SwapChain.h
+++ SwapChain.h
@@ -3,33 +3,57 @@
 #include "GraphicAPI/MeshResources.h"
 
 
-class SwapChain
+typedef void*	WindowHandler;
+
+
+struct SwapChainDescriptor
+{
+	WindowHandler			WindowHandle;
+	unsigned short			Width;
+	unsigned short			Height;
+	ResourceFormat			Format;
+	uint8					SamplesCount;
+	uint8					SamplesQuality;
+	bool					AllowFullscreen;
+	uint8					NumBuffers;
+
+
+	SwapChainDescriptor()
+		:	SamplesCount( 1 )
+		,	SamplesQuality( 0 )
+		,	AllowFullscreen( true )
+		,	Format( ResourceFormat::RESOURCE_FORMAT_R8G8B8A8_UNORM )
+		,	WindowHandle( nullptr )
+		,	NumBuffers( 1 )
+	{}
+};
+
+
+
+class SwapChain : public ResourceObject
 {
 	RTTR_ENABLE()
 protected:
 
-	RenderTargetObject*		m_renderTarget;
+	ResourcePtr< RenderTargetObject >	m_renderTarget;
 
 protected:
 	SwapChain( RenderTargetObject* windowRT )
-		:	m_renderTarget( windowRT )
+		:	ResourceObject( WRONG_ID )
+		,	m_renderTarget( windowRT )
 	{
 		assert( windowRT );
-		m_renderTarget->AddAssetReference();
 	}
 
 public:
 	virtual ~SwapChain()
-	{
-		m_renderTarget->DeleteAssetReference();
-		m_renderTarget = nullptr;
-	}
+	{}
 
 
 	virtual void			Present			( int syncInterval )					= 0;
 	virtual void			Resize			( uint16 newWidth, uint16 newHeight )	= 0;
 
 
-	RenderTargetObject*		GetRenderTarget	()		{ return m_renderTarget; }
+	ResourcePtr< RenderTargetObject >		GetRenderTarget	()		{ return m_renderTarget; }
 };
 
